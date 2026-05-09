50b7b9ea33dfc518a4edfc3e128676987108a90e
Jcb-Jcb <doublej7565@gmail.com>
2026-05-09 18:12:51 -0500

    Made into verilog

--- src/spi_internal.v
+++ src/spi_internal.v
@@ -26,55 +26,55 @@
 // ui_in[2] - MISO
 // uo_out[5] - SCK
 
-import spi_pkg::*;
-
 module spi_internal #(
-    parameter DATA_W = 8,
-    parameter ADDR_W = 16
+    parameter DATA_W     = 8,
+    parameter ADDR_W     = 16,
+    parameter ADDR_BIT_W = 4
 ) (
-    input logic              clk,
-    input logic              reset_n,
-    input logic              in_valid_i,
-    input logic [DATA_W-1:0] data_i,
-    input logic [ADDR_W-1:0] address_i,
-
-    input command_t          command_i, //0 for read, 1 for write
+    input wire              clk,
+    input wire              reset_n,
+    input wire              in_valid_i,
+    input wire              command_i, //0 for read, 1 for write
+    input wire [DATA_W-1:0] data_i,
+    input wire [ADDR_W-1:0] address_i,
+    input wire              miso, //data from external mem
     
-    output logic              out_valid_o, //for read
-    output logic [DATA_W-1:0] data_o,
-    output logic              done_o, //for write, idk if needed
-
-    spi_if.master             spi_bundle
+    output reg               out_valid_o, //for read
+    output reg  [DATA_W-1:0] data_o,
+    output reg               done_o, //for write, idk if needed
+    output wire              sclk,
+    output reg               cs, //active low, activates slave
+    output reg               mosi //data from asic
 );
 
-localparam logic [DATA_W-1:0] READ_CMD         = 8'h03; // we expect data immeditly after we give address (low spi_clk)
-localparam logic [DATA_W-1:0] FAST_READ_CMD    = 8'h0B; //we give SRAM dmmy cyles so it has time to read (high spi clk)
-localparam logic [DATA_W-1:0] WRITE_CMD        = 8'h02;
-localparam logic [DATA_W-1:0] WRITE_STATUS_CMD = 8'h01;
-localparam logic [DATA_W-1:0] READ_STATUS_CMD  = 8'h05;
+localparam [2:0]        NUM_STATES = 3'd5;
 
-localparam int ADDR_BIT_W = $clog2(ADDR_W); 
+localparam [DATA_W-1:0] READ_CMD         = 8'h03; // we expect data immeditly after we give address (low spi_clk)
+localparam [DATA_W-1:0] FAST_READ_CMD    = 8'h0B; //we give SRAM dmmy cyles so it has time to read (high spi clk)
+localparam [DATA_W-1:0] WRITE_CMD        = 8'h02;
+localparam [DATA_W-1:0] WRITE_STATUS_CMD = 8'h01;
+localparam [DATA_W-1:0] READ_STATUS_CMD  = 8'h05;
 
-typedef enum logic [4:0] {IDLE, SHIFT_COMMAND, SHIFT_ADDR, READ , WRITE} state_t;
-state_t current_state, next_state;
+localparam [NUM_STATES-1:0] IDLE          = 3'd0;
+localparam [NUM_STATES-1:0] SHIFT_COMMAND = 3'd1;
+localparam [NUM_STATES-1:0] SHIFT_ADDR    = 3'd2;
+localparam [NUM_STATES-1:0] READ          = 3'd3;
+localparam [NUM_STATES-1:0] WRITE         = 3'd4;
 
-logic [ADDR_BIT_W-1:0] counter_q, counter_d; //use for both addr, and command
-logic [DATA_W-1:0]     command_val_d, command_val_q;
-logic [ADDR_W-1:0]     address_q, address_d;
-logic [DATA_W-1:0]     data_q, data_d;
-logic [DATA_W-1:0]     data_out_d, data_out_q;
-logic                  cs_d;
-logic                  mosi_d;
-logic                  done_d;
-logic                  out_valid_d;
+reg [NUM_STATES-1:0] current_state, next_state;
 
-command_t              command_q, command_d;
+reg [ADDR_BIT_W-1:0] counter_q, counter_d; //use for both addr, and command
+reg [DATA_W-1:0]     command_val_d, command_val_q;
+reg [ADDR_W-1:0]     address_q, address_d;
+reg [DATA_W-1:0]     data_q, data_d;
+reg [DATA_W-1:0]     data_out_d, data_out_q;
+reg                  cs_d;
+reg                  mosi_d;
+reg                  done_d;
+reg                  out_valid_d;
+reg                  command_q, command_d;
 
-
-//TODO: If valid & command_i = 0, then do fast read & send data out
-//TODO: If valid & command_i = 1, then do write
 //TODO: Decide if fast read for normal is better
-//TODO: make verilog
 
 // FSM = IDLE -> READ || WRITE -> IDLE
 // On transition idle -> read || write CS = 0
@@ -86,38 +86,44 @@ command_t              command_q, command_d;
 //so we can techinally get as many bytes as we want, as long as they are consecutive
 //non sequential addresses need seperate reads
 
-assign spi_bundle.sclk = clk;
+assign sclk = cs ? 1'b0 : ~clk;
 
-always_ff @(posedge clk or negedge reset_n) begin
+always @(posedge clk or negedge reset_n) begin
     if(!reset_n) begin
         current_state <= IDLE;
-        spi_bundle.cs <= '0;
-        counter_q     <= '0;
-        done_o        <= '0;
-        out_valid_o   <= '0;
+        cs            <= 1'b1;
+        mosi          <= 1'b0;
+        counter_q     <= {ADDR_BIT_W{1'b0}};
+        command_q     <= 1'b0;
+        command_val_q <= {DATA_W{1'b0}};
+        address_q     <= {ADDR_W{1'b0}};
+        data_q        <= {DATA_W{1'b0}};
+        data_out_q    <= {DATA_W{1'b0}};
+        data_o        <= {DATA_W{1'b0}};
+        done_o        <= 1'b0;
+        out_valid_o   <= 1'b0;
     end else begin
-        current_state   <= next_state;
-        spi_bundle.cs   <= cs_d;
-        command_q       <= command_d;
-        command_val_q   <= command_val_d;
-        counter_q       <= counter_d;
-        spi_bundle.mosi <= mosi_d;
-        address_q       <= address_d;
-        data_q          <= data_d;
-        data_out_q      <= data_out_d;
-        out_valid_o     <= out_valid_d;
-        done_o          <= done_d;
+        current_state <= next_state;
+        cs            <= cs_d;
+        command_q     <= command_d;
+        command_val_q <= command_val_d;
+        counter_q     <= counter_d;
+        mosi          <= mosi_d;
+        address_q     <= address_d;
+        data_q        <= data_d;
+        data_out_q    <= data_out_d;
+        data_o        <= data_out_d;
+        out_valid_o   <= out_valid_d;
+        done_o        <= done_d;
     end
 end
 
-assign data_o = data_out_q;
-
-always_comb begin
+always @(*) begin
     //defaults
     mosi_d        = 1'b0;
     out_valid_d   = 1'b0;
     done_d        = 1'b0;
-    cs_d          = spi_bundle.cs;
+    cs_d          = cs;
     command_d     = command_q;
     counter_d     = counter_q;
     command_val_d = command_val_q;
@@ -126,7 +132,7 @@ always_comb begin
     data_d        = data_q;
     data_out_d    = data_out_q;
 
-    unique case(current_state)
+    case(current_state)
         IDLE : begin
                 cs_d = 1'b1;
                 if(in_valid_i) begin
@@ -135,7 +141,7 @@ always_comb begin
                     command_d     = command_i;
                     address_d     = address_i;
                     data_d        = data_i;
-                    command_val_d = (command_i == WRITE_T) ? WRITE_CMD : READ_CMD;
+                    command_val_d = command_i ? WRITE_CMD : READ_CMD;
                 end
             end 
         
@@ -144,7 +150,7 @@ always_comb begin
             command_val_d = command_val_q << 1;
 
             if(counter_q == DATA_W-1) begin
-                counter_d  = '0;
+                counter_d  = {ADDR_BIT_W{1'b0}};
                 next_state = SHIFT_ADDR;
             end else begin
                 counter_d = counter_q + 1;
@@ -156,18 +162,18 @@ always_comb begin
             address_d = address_q << 1;
 
             if(counter_q == ADDR_W-1) begin
-                counter_d  = '0;
-                next_state = (command_q == WRITE_T) ? WRITE : READ;
+                counter_d  = {ADDR_BIT_W{1'b0}};
+                next_state = command_q ? WRITE : READ;
             end else begin
                 counter_d = counter_q + 1;
             end
         end
 
         READ : begin // listen to data
-            data_out_d = {data_out_q[DATA_W-1:1], spi_bundle.miso};
+            data_out_d = {data_out_q[DATA_W-1:1], miso};
 
             if(counter_q == DATA_W-1) begin
-                counter_d   = '0;
+                counter_d   = {ADDR_BIT_W{1'b0}};
                 cs_d        = 1'b1; // not listening anymore
                 out_valid_d = 1'b1;
                 next_state  = IDLE; 
@@ -181,7 +187,7 @@ always_comb begin
             data_d = data_q << 1;
 
             if(counter_q == DATA_W-1) begin
-                counter_d  = '0;
+                counter_d  = {ADDR_BIT_W{1'b0}};
                 cs_d       = 1'b1; // not listening anymore
                 done_d     = 1'b1;
                 next_state = IDLE;
@@ -192,4 +198,4 @@ always_comb begin
     endcase
 end
     
-endmodule : spi_internal
+endmodule

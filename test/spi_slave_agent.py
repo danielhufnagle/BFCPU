675c3663d5bdeae554c3823d061c9a544c36fab5
Jcb-Jcb <doublej7565@gmail.com>
2026-05-10 16:42:30 -0500

    Updated the slave to be a closer model to the spi-ram-emu github (the firware we will actually be using)

--- test/spi_slave_agent.py
+++ test/spi_slave_agent.py
@@ -1,9 +1,15 @@
 # spi_slave_agent.py
 
-from cocotbext.spi import SpiBus, SpiConfig, SpiSlaveBase
+from cocotb.triggers import ReadOnly
+from cocotbext.spi import SpiBus, SpiConfig, SpiFrameError, SpiSlaveBase
 
 
 class SpiSlave(SpiSlaveBase):
+    READ_CMD = 0x03
+    WRITE_CMD = 0x02
+    FAST_READ_CMD = 0x0B
+    MEMORY_SIZE = 1 << 16
+
     def __init__(self, bus, config=None):
         self._config = config or SpiConfig(
             word_width=8,
@@ -14,24 +20,65 @@ class SpiSlave(SpiSlaveBase):
         )
 
         self.rx_words = []
-        self.tx_queue = []
+        self.memory = {}
 
         super().__init__(bus)
 
-    def add_response(self, word):
-        self.tx_queue.append(word)
+    def read_memory(self, address):
+        return self.memory.get(address % self.MEMORY_SIZE, 0)
+
+    def write_memory(self, address, data):
+        self.memory[address % self.MEMORY_SIZE] = data & 0xFF
+
+    async def _cs_active(self):
+        await ReadOnly()
+        cs_value = int(self._cs.value)
+        return cs_value == 0 if self._config.cs_active_low else cs_value == 1
 
     async def _transaction(self, frame_start, frame_end):
         await frame_start
         self.idle.clear()
 
-        tx_word = self.tx_queue.pop(0) if self.tx_queue else 0x00
-        rx_word = int(await self._shift(8, tx_word=tx_word))
+        try:
+            command = int(await self._shift(8))
+            address = int(await self._shift(16))
+            self.rx_words.append(command)
+            self.rx_words.append((address >> 8) & 0xFF)
+            self.rx_words.append(address & 0xFF)
 
-        self.rx_words.append(rx_word)
+            if command == self.WRITE_CMD:
+                await self._write_stream(address)
+            elif command == self.READ_CMD:
+                await self._read_stream(address)
+            elif command == self.FAST_READ_CMD:
+                await self._shift(8)
+                await self._read_stream(address)
+            else:
+                await frame_end
+        except SpiFrameError:
+            pass
+        finally:
+            self.idle.set()
 
-        await frame_end
-        self.idle.set()
+    async def _write_stream(self, address):
+        while await self._cs_active():
+            try:
+                data = int(await self._shift(8))
+            except SpiFrameError:
+                break
+
+            self.write_memory(address, data)
+            self.rx_words.append(data)
+            address = (address + 1) % self.MEMORY_SIZE
+
+    async def _read_stream(self, address):
+        while await self._cs_active():
+            try:
+                await self._shift(8, tx_word=self.read_memory(address))
+            except SpiFrameError:
+                break
+
+            address = (address + 1) % self.MEMORY_SIZE
 
 
 class SpiSlaveAgent:
@@ -49,7 +96,17 @@ class SpiSlaveAgent:
         self.slave = SpiSlave(self.bus)
 
     def queue_response(self, word):
-        self.slave.add_response(word)
+        self.slave.write_memory(0, word)
+
+    def load_memory(self, start_address, data):
+        for offset, byte in enumerate(data):
+            self.write_memory(start_address + offset, byte)
+
+    def read_memory(self, address):
+        return self.slave.read_memory(address)
+
+    def write_memory(self, address, data):
+        self.slave.write_memory(address, data)
 
     def get_received_words(self):
         return self.slave.rx_words

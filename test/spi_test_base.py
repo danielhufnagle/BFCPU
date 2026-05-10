00468a89d5cf83afa22c23d1dcdf2740ff2f9b69
Jcb-Jcb <doublej7565@gmail.com>
2026-05-10 16:18:56 -0500

    updated test base to use custom parts & create spi slave

---
+++ test/spi_test_base.py
@@ -0,0 +1,59 @@
+from typing import TypeVar, Generic, Type
+from cocotb.triggers import RisingEdge, Timer
+
+from tb_utils.generic_drivers import GenericDriver
+from tb_utils.generic_sequence import GenericSequence
+from tb_utils.generic_monitor import GenericMonitor
+from tb_utils.generic_scoreboard import GenericScoreboard
+from tb_utils.generic_model import GenericModel
+from tb_utils.generic_checker import GenericChecker
+from tb_utils.abstract_transactions import AbstractTransaction
+
+from spi_sequence import SPISequence
+from spi_sequence_item import SPISequenceItem
+from spi_slave_agent import SpiSlaveAgent
+
+
+DriverT = TypeVar("DriverT", bound=GenericDriver)
+SequenceT = TypeVar("SequenceT", bound=GenericSequence)
+MonitorT = TypeVar("MonitorT", bound=GenericMonitor)
+ScoreboardT = TypeVar("ScoreboardT", bound=GenericScoreboard)
+ModelT = TypeVar("ModelT", bound=GenericModel)
+CheckerT = TypeVar("CheckerT", bound=GenericChecker)
+SeqItemT = TypeVar("SeqItemT", bound=AbstractTransaction)
+OutTransT = TypeVar("OutTransT", bound=AbstractTransaction)
+
+
+class GenericTestBase(
+    Generic[
+        DriverT, SeqItemT, SequenceT, MonitorT, OutTransT, ScoreboardT, ModelT, CheckerT
+    ]
+):
+    def __init__(
+        self,
+        dut,
+        driver: Type[DriverT] = GenericDriver,
+        sequence_item: Type[SeqItemT] = SPISequenceItem,
+        sequence: Type[SequenceT] = SPISequence,
+        monitor: Type[MonitorT] = GenericMonitor,
+        output_transaction: Type[OutTransT] = AbstractTransaction,
+        scoreboard: Type[ScoreboardT] = GenericScoreboard,
+        model: Type[ModelT] = GenericModel,
+        checker: Type[CheckerT] = GenericChecker,
+    ):
+        self.dut = dut
+
+        self.driver: DriverT = driver(dut=dut, seq_item_type=sequence_item)
+        self.sequence: SequenceT = sequence(driver=self.driver)
+        self.monitor: MonitorT = monitor(dut=dut, output_transaction=output_transaction)
+
+        self.scoreboard: ScoreboardT = scoreboard(
+            monitor=self.monitor, model=model(), checker=checker()
+        )
+        self.spi_slave = SpiSlaveAgent(dut)
+
+    async def wait_for_driver_done(self):
+        while await self.driver.busy():
+            await RisingEdge(self.dut.clk)
+
+        await Timer(1000, unit="ns")

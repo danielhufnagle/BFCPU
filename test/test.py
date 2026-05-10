d81e9cf5dbe7421c99afd0e8c2a436655756f907
Jcb-Jcb <doublej7565@gmail.com>
2026-05-10 17:02:11 -0500

    small test

--- test/test.py
+++ test/test.py
@@ -1,40 +1,37 @@
-# SPDX-FileCopyrightText: © 2024 Tiny Tapeout
-# SPDX-License-Identifier: Apache-2.0
-
 import cocotb
 from cocotb.clock import Clock
-from cocotb.triggers import ClockCycles
+from cocotb.triggers import ClockCycles, RisingEdge
+
+from spi_test_base import GenericTestBase as SPITestBase
+
+
+async def reset_dut(dut):
+    dut.rst_n.value = 0
+    dut.in_valid_i.value = 0
+    dut.command_i.value = 0
+    dut.data_i.value = 0
+    dut.address_i.value = 0
+    await ClockCycles(dut.clk, 5)
+    dut.rst_n.value = 1
+    await ClockCycles(dut.clk, 2)
 
 
 @cocotb.test()
-async def test_project(dut):
-    dut._log.info("Start")
+async def test_spi_write_and_read(dut):
+    clock = Clock(dut.clk, 10, unit="ns")
+    clock.start()
 
-    # Set the clock period to 10 us (100 KHz)
-    clock = Clock(dut.clk, 10, unit="us")
-    cocotb.start_soon(clock.start())
+    tb = SPITestBase(dut)
+    await reset_dut(dut)
 
-    # Reset
-    dut._log.info("Reset")
-    dut.ena.value = 1
-    dut.ui_in.value = 0
-    dut.uio_in.value = 0
-    dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 10)
-    dut.rst_n.value = 1
+    write_address = 0x1234
+    write_data = 0xA5
+    await tb.sequence.send_manual_write(data_i=write_data, address_i=write_address)
+    await RisingEdge(dut.done_o)
+    assert tb.spi_slave.read_memory(write_address) == write_data
 
-    dut._log.info("Test project behavior")
-
-    # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
-
-    # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 1)
-
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
-
-    # Keep testing the module by changing the input values, waiting for
-    # one or more clock cycles, and asserting the expected output values.
+    read_address = 0x4321
+    read_data = 0x3C
+    await tb.sequence.send_manual_read(data_i=read_data, address_i=read_address)
+    await RisingEdge(dut.out_valid_o)
+    assert int(dut.data_o.value) == read_data

8f995adcc6c9291bd19ad811213e28c9dd0f8e42
Jcb-Jcb <doublej7565@gmail.com>
2026-05-15 19:12:36 -0500

    Updated DUT instantiation

--- test/tb.sv
+++ test/tb.sv
@@ -8,6 +8,13 @@ module tb ();
   import spi_pkg::*;
   spi_if spi_bundle();
 
+  localparam int DATA_W             = 8;
+  localparam int ADDR_W             = 16;
+  localparam int BFCPU_TO_RAM_RATIO = 4;
+  localparam int BFCPU_CLK_HZ       = 50_000_000;
+  localparam int RP2040_SYS_HZ      = 125_000_000;
+  localparam int SYS_GAP_CYCLES     = 50;
+
   // Dump the signals to a FST file. You can view it with gtkwave or surfer.
   initial begin
     $dumpfile("tb.fst");
@@ -19,17 +26,21 @@ module tb ();
   logic        clk;
   logic        rst_n;
   logic        in_valid_i;
-  logic [7:0]  data_i;
-  logic [15:0] address_i;
+  logic [DATA_W-1:0] data_i;
+  logic [ADDR_W-1:0] address_i;
   logic        out_valid_o;
-  logic [7:0]  data_o;
+  logic [DATA_W-1:0] data_o;
   logic        done_o;
   
   command_t command_i;
 
   spi_internal #(
-    .DATA_W(8),
-    .ADDR_W(16)
+    .DATA_W(DATA_W),
+    .ADDR_W(ADDR_W),
+    .BFCPU_TO_RAM_RATIO(BFCPU_TO_RAM_RATIO),
+    .BFCPU_CLK_HZ(BFCPU_CLK_HZ),
+    .RP2040_SYS_HZ(RP2040_SYS_HZ),
+    .SYS_GAP_CYCLES(SYS_GAP_CYCLES)
   ) spi_interface (
     .clk(clk),
     .reset_n(rst_n),
